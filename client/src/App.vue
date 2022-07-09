2db5488ec0f0484fc30a1d9dbb443e0a52685313
Md Janay Alam <janayalam820@gmail.com>
2022-07-09 22:31:05 +0600

    feat: changed some components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <NavbarVue />
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import NavbarVue from "./components/Navbar.vue";
+
+export default {
+  name: 'App',
+  components: { NavbarVue },
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-size: 12px;
+  font-family: 'Roboto', 'Albert Sans', sans-serif;
 }
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navigation-bar">CycleX</nav>
+</template>
+
+<script>
+
+export default {
+    name: 'Navbar',
+    components: {},
+}
+</script>
+
+<style scoped>
+.navigation-bar {
+    background-color: #EEEEEE;
+    min-height: 55px;
+}
+</style>
