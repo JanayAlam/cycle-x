65eeaba76886c92d43746501a515b8bdd224519f
Md Janay Alam <janayalam820@gmail.com>
2022-07-09 22:30:45 +0600

    feat: implemented some request models

---
+++ src/api/models/req-models/auth/login-req-model.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+    email: Joi.string()
+        .email({
+            minDomainSegments: 2,
+            tlds: {
+                allow: ['com', 'org', 'net'],
+            },
+        })
+        .min(5)
+        .max(150)
+        .required(),
+
+    password: Joi.string().min(4).required(),
+});
+
+/**
+ * login user schema validator.
+ * @param {Object} data The object which client sent.
+ * @returns {Object} The result object validated by joi.
+ */
+module.exports = (data) => {
+    const result = schema.validate(data);
+    result.value = data;
+    return result;
+};
