7b07bec1cf69a67e47253b60108709149ba17ae2
Md Janay Alam <janayalam820@gmail.com>
2022-07-11 20:32:38 +0600

    feat: added icons

--- client/src/main.js
+++ client/src/main.js
@@ -12,9 +12,25 @@ axios.defaults.baseURL = 'http://localhost:5000/api/v1';
 // vuex configuration
 require('@/store/subscriber');
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(fas);
+
 store
     .dispatch('attempt', localStorage.getItem('token'))
     .then(() => {
-        createApp(App).use(store).use(router).mount('#app');
+        createApp(App)
+            .component('font-awesome-icon', FontAwesomeIcon)
+            .use(store)
+            .use(router)
+            .mount('#app');
     })
     .catch((err) => console.log(err));
