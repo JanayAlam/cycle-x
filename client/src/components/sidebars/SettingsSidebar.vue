827f51c1942ed027fdd621d11f4ebf24ef026119
Md Janay Alam <janayalam820@gmail.com>
2022-07-16 01:51:23 +0600

    feat: implemented basic profile settings page

---
+++ client/src/components/sidebars/SettingsSidebar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="list-group">
+        <router-link :to="{ name: 'profile-settings' }" class="list-group-item list-group-item-action"
+            :class="{ 'active': active === 'profile-settings' }" aria-current="true">
+            <font-awesome-icon icon="fa-solid fa-user-gear" />
+            <span class="ms-2">Profile Details</span>
+        </router-link>
+        <router-link to="#" class="list-group-item list-group-item-action"
+            :class="{ 'active': active === 'verify-email' }">
+            <font-awesome-icon icon="fa-solid fa-user-check" />
+            <span class="ms-2">Verify Email Address</span>
+        </router-link>
+        <router-link to="#" class="list-group-item list-group-item-action"
+            :class="{ 'active': active === 'change-user' }">
+            <font-awesome-icon icon="fa-solid fa-at" />
+            <span class="ms-2">Change Email or NID</span>
+        </router-link>
+        <router-link to="#" class="list-group-item list-group-item-action"
+            :class="{ 'active': active === 'change-password' }">
+            <font-awesome-icon icon="fa-solid fa-key" />
+            <span class="ms-2">Change Password</span>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SettingsSidebar',
+    props: {
+        active: String,
+    },
+    setup(_props) {
+
+        return {};
+    },
+}
+</script>
+
+<style scoped>
+.list-group-item {
+    font-size: 0.95rem;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    /* text-align: center; */
+    border: 0;
+    background-color: transparent;
+    font-weight: bold;
+    border-radius: 5px;
+}
+
+.active,
+.list-group-item:hover {
+    background-color: #dff1fc;
+    color: #072A53;
+}
+
+.list-group-item:hover {
+    text-decoration: underline;
+}
+</style>
---
+++ client/src/views/profile/ProfileSettings.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="sidebar col-md-3 col-sm-4">
+                <SettingsSidebar active="profile-settings" />
+            </div>
+            <div class="col-md-9 col-sm-8">
+                <div v-if="isLoading">
+                    <div class="card" aria-hidden="true">
+                        <div class="card-body">
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                            <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder placeholder-glow col-6"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SettingsSidebar from '@/components/sidebars/SettingsSidebar.vue';
+import { reactive } from 'vue';
+
+export default {
+    name: 'ProfileSettings',
+    components: { SettingsSidebar },
+    setup() {
+        document.title = 'Settings';
+        const state = reactive({
+            isLoading: true,
+        });
+
+        return { ...state };
+    },
+};
+</script>
+
+<style scoped>
+</style>
