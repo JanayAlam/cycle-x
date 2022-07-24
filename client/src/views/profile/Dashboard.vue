ffb140f7b212710a5cbcf07525b95618b75c77d5
Anabilsarker <sarkeranabil@gmail.com>
2022-07-24 21:15:35 +0600

    Dashboard Development

--- client/src/views/profile/Dashboard.vue
+++ client/src/views/profile/Dashboard.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <div class="vstack gap-3 m-2 d-inline-flex bg-dark bg-gradient text-white">
-            <img class="rounded-circle" :src="profile.profilePhoto" alt="Profile Photo" height="110" width="130" />
-            <label class="text-center"> {{ profile.firstName }} </label>
-        </div>
-        <div class="border border-dark bg-gradient text-white">
-            <img class="" height="100" width="100"/>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-2">
+                <div class="vstack gap-3 m-2 d-inline-flex bg-dark bg-gradient text-white">
+                    <img class="rounded-circle" :src="profile.profilePhoto" alt="Profile Photo" height="110"
+                        width="130" />
+                    <label class="text-center"> {{ profile.firstName }} </label>
+                </div>
+            </div>
+            <div class="col-md-9 col-sm-3">
+                <div class="vstack border border-dark bg-gradient text-dark">
+                    <label class="text-center"> Card Type </label>
+                    <img src="../../../../public/default/card.png" height="100" width="200" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,41 +25,37 @@ import { computed } from "@vue/reactivity";
 import { useStore } from "vuex";
 
 export default
-{
-    name: "Dashboard",
-    components: { SettingsSidebar, ProfileDetailsForm },
-    setup()
     {
-        document.title = "Dashboard";
-        const store = useStore();
+        name: "Dashboard",
+        components: { SettingsSidebar, ProfileDetailsForm },
+        setup() {
+            document.title = "Dashboard";
+            const store = useStore();
 
-        const profile = computed(() => store.getters.getProfile);
+            const profile = computed(() => store.getters.getProfile);
 
-        const submitHandler = async () =>
-        {
-            try
-            {
-                await store.dispatch("changeProfileDetails",
-                {
-                    firstName: profile.value.firstName,
-                    lastName: profile.value.lastName,
-                    dob: profile.value.dob,
-                });
-                store.dispatch("pushNotification",
-                {
-                    type: "success",
-                    msg: "Profile details updated",
-                });
-            }
-            catch (e)
-            {
-                store.dispatch("pushNotification", { type: "danger", msg: e.message });
-            }
-        };
+            const submitHandler = async () => {
+                try {
+                    await store.dispatch("changeProfileDetails",
+                        {
+                            firstName: profile.value.firstName,
+                            lastName: profile.value.lastName,
+                            dob: profile.value.dob,
+                        });
+                    store.dispatch("pushNotification",
+                        {
+                            type: "success",
+                            msg: "Profile details updated",
+                        });
+                }
+                catch (e) {
+                    store.dispatch("pushNotification", { type: "danger", msg: e.message });
+                }
+            };
 
-        return { profile, submitHandler };
-    },
-};
+            return { profile, submitHandler };
+        },
+    };
 </script>
 
 <style scoped>
