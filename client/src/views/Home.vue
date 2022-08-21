2869867ce638fde2d1fea70851e60f05aee6c1d3
Anabilsarker <sarkeranabil@gmail.com>
2022-08-22 02:19:41 +0600

    Added QR Code Reader

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
         <div>
-            <GMapMap :center="center" :zoom="17" map-type-id="terrain" style="width: 100%; height: 100vh">
-                <GMapMarker :key="id" :position="center" />
+            <GMapMap :center="currentUserLocation" :zoom="17" map-type-id="terrain" style="width: 100%; height: 100vh">
+                <GMapCluster>
+                    <GMapMarker :key="id" :position="currentUserLocation" :clickable="true" :draggable="true" />
+
+                    <GMapMarker :key="'Banasree'" :position="cycleXHub1" :icon="hubIcon" :clickable="true"
+                        :draggable="false" />
+                    <GMapMarker :key="'Abul Hotel'" :position="cycleXHub2" :icon="hubIcon" :clickable="true"
+                        :draggable="false" />
+                    <GMapMarker :key="'Aarong Gulshan'" :position="cycleXHub3" :icon="hubIcon" :clickable="true"
+                        :draggable="false" />
+                </GMapCluster>
             </GMapMap>
         </div>
+        <div v-if="isVisible" class="QR-code-reader">
+            <div>
+                <p>{{ error }}</p>
+                <h1 class="bg-white">{{ decodedString }}</h1>
+                <qrcode-stream @init="OnInit" @decode="onDecode"></qrcode-stream>
+            </div>
+        </div>
         <div class="position">
-            <div >
+            <div>
                 <button @click="getLocation()">Get Location</button>
+                <button @click="getQRCode()">Camera</button>
             </div>
         </div>
     </div>
@@ -17,10 +34,11 @@
 <script>
 import { ref } from 'vue';
 import { mapGetters } from 'vuex';
+import { QrcodeStream } from 'vue3-qrcode-reader';
 
 export default {
     name: 'Home',
-    components: {},
+    components: { QrcodeStream },
     computed: {
         ...mapGetters(['isAuthenticated', 'getUser', 'getProfile']),
     },
@@ -34,9 +52,20 @@ export default {
     },
     data() {
         return {
-            latitude: 0.0,
-            longitude: 0.0,
-            center: { lat: 51.093048, lng: 6.842120 },
+            //QR Code Variables
+            error: '',
+            decodedString: '',
+            isVisible: false,
+
+            //hubIcon: '../../../public/default/CycleX-Hub-Icon.png',
+            hubIcon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png',
+            currentUserLocation: { lat: 23.76850367011616, lng: 90.42554965500942 },
+            rootLocation: { lat: 23.76850367011616, lng: 90.42554965500942 },
+
+            cycleXHub1: { lat: 23.764582527423773, lng: 90.42913999532198 },
+            cycleXHub2: { lat: 23.75491137184827, lng: 90.41557908203598 },
+            cycleXHub3: { lat: 23.77023019851235, lng: 90.40962595691866 },
+
             id: '3a17f42a4efce004'
         }
     },
@@ -45,14 +74,44 @@ export default {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
                     position => {
-                        this.center.lat = position.coords.latitude;
-                        this.center.lng = position.coords.longitude;
+                        this.currentUserLocation.lat = position.coords.latitude;
+                        this.currentUserLocation.lng = position.coords.longitude;
                     },
                     error => {
                         console.log(error.message);
                     },
                 )
             }
+        },
+        async OnInit(promise) {
+            try {
+                const { capabilities } = await promise
+
+                // successfully initialized
+            } catch (error) {
+                if (error.name === 'NotAllowedError') {
+                    this.error = 'user denied camera access permisson';
+                } else if (error.name === 'NotFoundError') {
+                    this.error = 'no suitable camera device installed';
+                } else if (error.name === 'NotSupportedError') {
+                    this.error = 'page is not served over HTTPS (or localhost)';
+                } else if (error.name === 'NotReadableError') {
+                    this.error = 'maybe camera is already in use';
+                } else if (error.name === 'OverconstrainedError') {
+                    this.error = 'did you requested the front camera although there is none?';
+                } else if (error.name === 'StreamApiNotSupportedError') {
+                    this.error = 'browser seems to be lacking features';
+                }
+            }
+            finally {
+                // hide loading indicator
+            }
+        },
+        onDecode(decodedString) {
+            this.decodedString = decodedString;
+        },
+        getQRCode() {
+            this.isVisible = true;
         }
     }
 }
@@ -79,5 +138,11 @@ export default {
     bottom: 10%;
     left: 91%;
     font-size: large;
+}
+
+.QR-code-reader {
+    position: absolute;
+    bottom: 20%;
+    left: 10%;
 }
 </style>
