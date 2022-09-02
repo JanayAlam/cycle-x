ec75f8b72e7a91b8b25dcc6256226ec4056ac421
JanayAlam <dev.janay@outlook.com>
2022-09-02 17:06:36 +0600

    feat: completed showing hubs in map

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,16 +2,27 @@
     <div v-if="isAuthenticated">
         <!--Google Map Implementation-->
         <div>
-            <GMapMap :center="currentUserLocation" :zoom="17" map-type-id="terrain" style="width: 100%; height: 100vh">
+            <GMapMap
+                :center="currentUserLocation"
+                :zoom="17"
+                map-type-id="terrain"
+                style="width: 100%; height: 100vh"
+            >
                 <GMapCluster>
-                    <GMapMarker :key="id" :position="currentUserLocation" :clickable="true" :draggable="true" />
-
-                    <GMapMarker :key="'Banasree'" :position="cycleXHub1" :icon="hubIcon" :clickable="true"
-                        :draggable="false" />
-                    <GMapMarker :key="'Abul Hotel'" :position="cycleXHub2" :icon="hubIcon" :clickable="true"
-                        :draggable="false" />
-                    <GMapMarker :key="'Aarong Gulshan'" :position="cycleXHub3" :icon="hubIcon" :clickable="true"
-                        :draggable="false" />
+                    <GMapMarker
+                        :key="id"
+                        :position="currentUserLocation"
+                        :clickable="true"
+                        :draggable="true"
+                    />
+                    <GMapMarker
+                        v-for="hub in hubs"
+                        :key="hub.name"
+                        :position="hub.loc"
+                        :icon="hubIcon"
+                        :clickable="true"
+                        :draggable="false"
+                    />
                 </GMapCluster>
             </GMapMap>
         </div>
@@ -19,7 +30,11 @@
         <div v-if="isQRStreamVisible" class="QR-code-reader-stream">
             <div class="text-end">
                 <button @click="closeQRCodeScanner()" class="btn btn-link">
-                    <img src="../../../public/default/stop-button-icon.png" height="30" width="30">
+                    <img
+                        src="../../../public/default/stop-button-icon.png"
+                        height="30"
+                        width="30"
+                    />
                 </button>
             </div>
 
@@ -31,10 +46,18 @@
         <div class="Location-QR-Scanner">
             <div class="vstack">
                 <button class="btn btn-link" @click="openQRCodeScanner()">
-                    <img src="../../../public/default/scan-qr-code-icon.jpg" height="100" width="100">
+                    <img
+                        src="../../../public/default/scan-qr-code-icon.jpg"
+                        height="100"
+                        width="100"
+                    />
                 </button>
                 <button class="btn btn-link" @click="getCurrentLocation()">
-                    <img src="../../../public/default/Location-Icon.png" height="80" width="80">
+                    <img
+                        src="../../../public/default/Location-Icon.png"
+                        height="80"
+                        width="80"
+                    />
                 </button>
             </div>
         </div>
@@ -45,6 +68,7 @@
 import { ref } from 'vue';
 import { mapGetters } from 'vuex';
 import { QrcodeStream } from 'vue3-qrcode-reader';
+import axios from 'axios';
 
 export default {
     name: 'Home',
@@ -52,11 +76,8 @@ export default {
     computed: {
         ...mapGetters(['isAuthenticated', 'getUser', 'getProfile']),
     },
-    created() { },
-    setup() {
-        const state = ref({})
-
-        return { state }
+    created() {
+        this.getAllHubs();
     },
     data() {
         return {
@@ -66,31 +87,33 @@ export default {
             isQRStreamVisible: false,
 
             //hubIcon: '../../../public/default/CycleX-Hub-Icon.png',
-            hubIcon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png',
-            currentUserLocation: { lat: 23.76850367011616, lng: 90.42554965500942 },
-
-            cycleXHub1: { lat: 23.764582527423773, lng: 90.42913999532198 },
-            cycleXHub2: { lat: 23.75491137184827, lng: 90.41557908203598 },
-            cycleXHub3: { lat: 23.77023019851235, lng: 90.40962595691866 },
-        }
+            hubIcon:
+                'https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png',
+            currentUserLocation: {
+                lat: 23.76850367011616,
+                lng: 90.42554965500942,
+            },
+            hubs: [],
+        };
     },
     methods: {
         getCurrentLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
-                    position => {
+                    (position) => {
                         this.currentUserLocation.lat = position.coords.latitude;
-                        this.currentUserLocation.lng = position.coords.longitude;
+                        this.currentUserLocation.lng =
+                            position.coords.longitude;
                     },
-                    error => {
+                    (error) => {
                         console.log(error.message);
-                    },
-                )
+                    }
+                );
             }
         },
         async OnInit(promise) {
             try {
-                const { capabilities } = await promise
+                const { capabilities } = await promise;
 
                 // successfully initialized
             } catch (error) {
@@ -103,13 +126,13 @@ export default {
                 } else if (error.name === 'NotReadableError') {
                     this.error = 'maybe camera is already in use';
                 } else if (error.name === 'OverconstrainedError') {
-                    this.error = 'did you requested the front camera although there is none?';
+                    this.error =
+                        'did you requested the front camera although there is none?';
                 } else if (error.name === 'StreamApiNotSupportedError') {
                     this.error = 'browser seems to be lacking features';
                 }
                 this.isQRStreamVisible = false;
-            }
-            finally {
+            } finally {
                 // hide loading indicator
             }
         },
@@ -124,9 +147,19 @@ export default {
         },
         parseJSON() {
             let cycle_id = JSON.parse(this.decodedString);
-        }
-    }
-}
+        },
+        async getAllHubs() {
+            const res = await axios.get('hubs/');
+            this.hubs = res.data.map((h) => {
+                return {
+                    name: h.name,
+                    loc: { lat: h.latitude, lng: h.longitude },
+                };
+            });
+            console.log(this.hubs);
+        },
+    },
+};
 </script>
 
 <style scoped>
